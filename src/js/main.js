67a6819a10c39ee8c5b9cad322399ffaf0e066ca
Nilesh Haldar <nileshhaldar98@gmail.com>
2025-03-31 11:24:09 +0530

    folder structure added

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initializeCodeCopy } from './utils/copyCode.js';
-import { initializeNavigation } from './utils/navigation.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeNavigation();
-  initializeCodeCopy();
-});
