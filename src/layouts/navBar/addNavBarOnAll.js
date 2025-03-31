f2e84e11bb0be203e21fa04c8051ef208bb0dc31
Aaryan Pandey <54701470+imaaryan@users.noreply.github.com>
2025-03-31 17:37:10 +0530

    Finel Folder Structure

---
+++ src/layouts/navBar/addNavBarOnAll.js
@@ -0,0 +1,4 @@
+import "../../../style.css";
+import { navBar } from "./navBar.js";
+
+document.querySelector(".navbar").innerHTML = navBar;
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,5 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  root: '.',
-  build: {
-    outDir: 'dist',
-    lib: {
-      entry: './js/main.js',
-      name: 'SwingUI',
-      fileName: 'swingui'
-    }
-  },
-  publicDir: 'public',
-  server: {
-    port: 3000
-  },
-  plugins: [
-    tailwindcss(),
-  ]
-
+  plugins: [tailwindcss()],
 });
