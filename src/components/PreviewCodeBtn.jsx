ed243ba3d28d1efc74ddf57b989941e76b9877cd
Yasho321 <yashovardhans321@gmail.com>
2025-04-11 17:02:56 +0530

    Fix component look and toggle btns

--- src/components/PreviewCodeBtn.jsx
+++ src/components/PreviewCodeBtn.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { useTheme } from '../context/ThemeContext.jsx';
 
 function PreviewCodeBtn({ showCode, setShowCode }) {
+  const { darkMode } = useTheme(); 
   return (
     <div className="flex  items-center justify-end gap-3 mb-3">
-      <div className='bg-[#02111A] px-3 py-1 rounded-lg'>
+      <div className={`${darkMode ? 'bg-[var(--dark-navbar-bg)] text-[var(--color-text-dark)] ' : 'bg-[var(--light-navbar-bg)] text-[var(--color-text)] '} px-3 py-1 rounded-lg`}>
       <button
         onClick={() => setShowCode(false)}
-        className={`px-5 py-2 text-sm font-medium rounded-lg shadow-md transition-all duration-300 ${
+        className={`px-5 py-2 text-sm font-medium rounded-lg  transition-all duration-300 ${
           !showCode
-            ? "bg-[#09151C] text-[#FFFFFF]  shadow-lg"
-            : "text-[#A2A2A2]   dark:text-[#A2A2A2]"
+            ? ` ${darkMode ? 'bg-[var(--dark-bg)]  text-white' : 'bg-[var(--light-bg)]  text-black'} shadow-lg`
+            : `${darkMode ? ' text-gray-400' : ' text-gray-600 '}`
         }`}
       >
         Preview
       </button>
       <button
         onClick={() => setShowCode(true)}
-        className={`px-5 py-2 text-sm font-medium rounded-lg shadow-md transition-all duration-300 ${
+        className={`px-5 py-2 text-sm font-medium rounded-lg transition-all duration-300 ${
           showCode
-            ? "bg-[#09151C] text-[#FFFFFF] shadow-lg"
-            : "text-[#A2A2A2]   dark:text-[#A2A2A2] "
+            ? `${darkMode ? 'bg-[var(--dark-bg)] text-white ' : 'bg-[var(--light-bg)] text-black '} shadow-lg`
+            : `${darkMode ? ' text-gray-400 ' : ' text-gray-600 '}   `
         }`}
       >
         Code
