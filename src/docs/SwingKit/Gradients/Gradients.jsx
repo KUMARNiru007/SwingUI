93b21525c13e854239f8853b06df56234772cd9f
Aaryan Pandey <54701470+imaaryan@users.noreply.github.com>
2025-04-14 21:53:28 +0530

    gradinet changes

--- src/docs/SwingKit/Gradients/Gradients.jsx
+++ src/docs/SwingKit/Gradients/Gradients.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import HoverAndCopyBlock from '../../components/HoverAndCopyBlock/hoverAndCopyBlock';
-import gradients from './graidentsData.js';
+// import gradients from './graidentsData.js';
 import { useTheme } from '../../../context/ThemeContext.jsx';
 import './style.css';
 import '../TextGradients/style.css';
+
+
+const gradients = [
+  {
+    id: 1,
+    name: 'swing-ocean-gradient',
+    class: 'swing-ocean-gradient',
+  },
+  {
+    id: 2,
+    name: 'swing-fire-gradient',
+    class: 'swing-fire-gradient',
+  },
+  {
+    id: 3,
+    name: 'swing-love-gradient',
+    class: 'swing-love-gradient',
+  },
+  {
+    id: 4,
+    name: 'swing-deepsea-gradient',
+    class: 'swing-deepsea-gradient',
+  },
+  {
+    id: 5,
+    name: 'swing-royal-gradient',
+    class: 'swing-royal-gradient',
+  },
+  {
+    id: 6,
+    name: 'swing-peach-gradient',
+    class: 'swing-peach-gradient',
+  },
+];
 
 const GradientGrid = () => {
   const { darkMode } = useTheme();
