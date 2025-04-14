60b56f7e12cf2751a03f3b1cde8bc8a73cd2f799
Yasho321 <yashovardhans321@gmail.com>
2025-04-14 17:51:29 +0530

    Button documentation added

--- src/docs/internal-components/Button/ButtonData.js
+++ src/docs/internal-components/Button/ButtonData.js
@@ -1,27 +1,27 @@
 const buttonData = [
   {
     id: 'swing_normal',
-    section: 'Swing Button',
-    label: 'Normal',
-    description: 'A swing button with a vibrant gradient background.',
-    code: `<button>
-  <a href="#" class="group swing-ocean-gradient px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Normal</a>
-</button>`,
-  },
-  {
-    id: 'swing_hover',
-    section: 'Swing Button',
-    label: 'Hover Me',
-    description: 'A swing button that shows an alternate gradient on hover.',
+    section: '',
+    label: 'Normal Button',
+    description: 'A gradient button with smooth hover transitions, changing to a different gradient on hover for an elegant effect.',
     code: `<button>
   <a href="#" class="group swing-ocean-gradient hover:swing-ocean-gradient px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Hover Me</a>
 </button>`,
   },
+//   {
+//     id: 'swing_hover',
+//     section: 'Swing Button',
+//     label: 'Hover Me',
+//     description: 'A swing button that shows an alternate gradient on hover.',
+//     code: `<button>
+//   <a href="#" class="group swing-ocean-gradient hover:swing-ocean-gradient px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Hover Me</a>
+// </button>`,
+//   },
   {
     id: 'swing_call',
-    section: 'Swing Button',
-    label: 'Call Now',
-    description: 'A swing button featuring a phone icon for immediate calls.',
+    section: '',
+    label: 'Button with Icon',
+    description: 'A swing button that combines a gradient effect and a phone icon, adding a sleek and functional design.',
     code: `<button>
   <a href="#" class="group swing-ocean-gradient hover:swing-ocean-gradient px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">
     <span>
@@ -32,9 +32,9 @@ const buttonData = [
   },
   {
     id: 'swing_getStarted',
-    section: 'Swing Button',
-    label: 'Get Started',
-    description: 'An animated button with a sliding arrow effect.',
+    section: '',
+    label: 'Icon On Hover ',
+    description: 'An engaging button that slides text and reveals an arrow icon upon hover, providing clear direction and smooth transitions.',
     code: `<button>
   <a href="#" class="group swing-ocean-gradient hover:swing-ocean-gradient px-10 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden transition-all duration-200">
     <div class="relative flex items-center">
@@ -46,20 +46,20 @@ const buttonData = [
   </a>
 </button>`,
   },
-  {
-    id: 'animated_normal',
-    section: 'Animated Swing Button',
-    label: 'Normal',
-    description: 'An animated swing button with continuous gradient motion.',
-    code: `<button>
-  <a href="#" class="swing-ocean-gradient-animate px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Normal</a>
-</button>`,
-  },
+//   {
+//     id: 'animated_normal',
+//     section: 'Animated Swing Button',
+//     label: 'Normal',
+//     description: 'An animated swing button with continuous gradient motion.',
+//     code: `<button>
+//   <a href="#" class="swing-ocean-gradient-animate px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Normal</a>
+// </button>`,
+//   },
   {
     id: 'animated_hover',
-    section: 'Animated Swing Button',
-    label: 'Hover Me',
-    description: 'An animated button that changes its gradient on hover.',
+    section: '',
+    label: 'Animated Swing Button ',
+    description: "A dynamic button with a continuously moving gradient effect, enhancing the visual appeal and drawing users' attention.",
     code: `<button>
   <a href="#" class="swing-ocean-gradient-animate hover:swing-ocean-gradient px-8 py-2 rounded-md text-white inline-flex items-center justify-center overflow-hidden">Hover Me</a>
 </button>`,
