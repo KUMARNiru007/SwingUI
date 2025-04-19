f04b42ec82bde05f462084183cb8a736596e3ba9
rohitisog <rohitisog@gmail.com>
2025-04-19 16:42:56 +0530

    resolved merge conflicts after stash pop

--- src/Layout/Components.jsx
+++ src/Layout/Components.jsx
@@ -57,16 +57,9 @@ const Components = () => {
             <Route path='header' element={<Header />} />
             {/* <Route path='hero' element={<Hero />} /> */}
             <Route path='image-gallery' element={<ImageGallery />} />
-<<<<<<< HEAD
-            <Route path='LevitatingAvatars' element={<LevitatingAvatars />} />
-=======
-<<<<<<< HEAD
             <Route path='LevitatingAvatars ' element={<LevitatingAvatars />} />
-=======
             <Route path='image-accordion' element={<ImageAccordions />} />
             <Route path='word-rotating' element={<WordRotating />} />
->>>>>>> ed0f8e3 (2 comp added)
->>>>>>> 756a41e (2 comp added)
             <Route path='navbar' element={<Navbar />} />
             {/* <Route path='popups' element={<PopUps />} /> */}
             <Route path='tabs' element={<Tabs />} />
